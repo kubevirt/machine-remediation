198f030dbd7ac8b383a12a4836ee75728f459250
Andrew Beekhof <andrew@beekhof.net>
2019-08-23 12:05:51 +1000

    Update dependencies of operator-lifecycle-manager to include pkg/version

--- vendor/github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1/catalogsource_types.go
+++ vendor/github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1/catalogsource_types.go
@@ -5,12 +5,10 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-
-	"github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators"
 )
 
 const (
-	CatalogSourceCRDAPIVersion = operators.GroupName + "/" + GroupVersion
+	CatalogSourceCRDAPIVersion = GroupName + "/" + GroupVersion
 	CatalogSourceKind          = "CatalogSource"
 )
 
@@ -27,6 +25,11 @@ const (
 	// SourceTypeGrpc specifies a CatalogSource that can use an operator registry image to generate a
 	// registry-server or connect to a pre-existing registry at an address.
 	SourceTypeGrpc SourceType = "grpc"
+)
+
+const (
+	CatalogSourceConfigMapError      ConditionReason = "ConfigMapError"
+	CatalogSourceRegistryServerError ConditionReason = "RegistryServerError"
 )
 
 type CatalogSourceSpec struct {
@@ -75,22 +78,41 @@ func (s *RegistryServiceStatus) Address() string {
 	return fmt.Sprintf("%s.%s.svc.cluster.local:%s", s.ServiceName, s.ServiceNamespace, s.Port)
 }
 
+type GRPCConnectionState struct {
+	Address           string      `json:"address,omitempty"`
+	LastObservedState string      `json:"lastObservedState"`
+	LastConnectTime   metav1.Time `json:"lastConnect,omitempty"`
+}
+
 type CatalogSourceStatus struct {
+	// A human readable message indicating details about why the ClusterServiceVersion is in this condition.
+	// +optional
+	Message string `json:"message,omitempty"`
+	// Reason is the reason the Subscription was transitioned to its current state.
+	// +optional
+	Reason ConditionReason `json:"reason,omitempty"`
+
 	ConfigMapResource     *ConfigMapResourceReference `json:"configMapReference,omitempty"`
 	RegistryServiceStatus *RegistryServiceStatus      `json:"registryService,omitempty"`
-	LastSync              metav1.Time                 `json:"lastSync,omitempty"`
+	GRPCConnectionState   *GRPCConnectionState        `json:"connectionState,omitempty"`
 }
 
 type ConfigMapResourceReference struct {
-	Name      string `json:"name"`
-	Namespace string `json:"namespace"`
+	Name            string      `json:"name"`
+	Namespace       string      `json:"namespace"`
+	UID             types.UID   `json:"uid,omitempty"`
+	ResourceVersion string      `json:"resourceVersion,omitempty"`
+	LastUpdateTime  metav1.Time `json:"lastUpdateTime,omitempty"`
+}
 
-	UID             types.UID `json:"uid,omitempty"`
-	ResourceVersion string    `json:"resourceVersion,omitempty"`
+func (r *ConfigMapResourceReference) IsAMatch(object *metav1.ObjectMeta) bool {
+	return r.UID == object.GetUID() && r.ResourceVersion == object.GetResourceVersion()
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +genclient
+
+// CatalogSource is a repository of CSVs, CRDs, and operator packages.
 type CatalogSource struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata"`
@@ -106,7 +128,17 @@ func (c *CatalogSource) Address() string {
 	return c.Status.RegistryServiceStatus.Address()
 }
 
+func (c *CatalogSource) SetError(reason ConditionReason, err error) {
+	c.Status.Reason = reason
+	c.Status.Message = ""
+	if err != nil {
+		c.Status.Message = err.Error()
+	}
+}
+
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CatalogSourceList is a repository of CSVs, CRDs, and operator packages.
 type CatalogSourceList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
