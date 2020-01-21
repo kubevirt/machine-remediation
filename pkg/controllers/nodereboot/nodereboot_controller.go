fd6055201b37de2f31f1071bc1c3d83447a300d5
Andrew Beekhof <andrew@beekhof.net>
2020-01-21 11:18:47 +1100

    Save Node labels and annotations so they can be restored once it returns
    Signed-off-by: abeekhof <abeekhof@redhat.com>

--- pkg/controllers/nodereboot/nodereboot_controller.go
+++ pkg/controllers/nodereboot/nodereboot_controller.go
@@ -111,6 +111,8 @@ func (r *ReconcileNodeReboot) Reconcile(request reconcile.Request) (reconcile.Re
 		Spec: mrv1.MachineRemediationSpec{
 			MachineName: machine.Name,
 			Type:        mrv1.RemediationTypeReboot,
+			SavedAnnotations: machine.Annotations,
+			SavedLabels: machine.Labels,
 		},
 	}
 
